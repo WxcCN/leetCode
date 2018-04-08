e4c9399e4864743b4f90421c8a2624c7ee1b9f2c
nova <357991863@qq.com>
2018-04-08 20:07:48 +0800

    first commit

---
+++ src/main/java/algorithms/t1.java
@@ -0,0 +1,26 @@
+package algorithms;
+
+/**
+ * 第一题
+ *
+ * @auther Nova wangxingchen
+ * @create 18/4/8
+ */
+public class t1 {
+    public static void main(String[] args) {
+        int nums[] = {2, 7, 11, 15};
+        int target = 9;
+    }
+    public int[] twoSum(int[] nums, int target) {
+        int a[] = new int[target];
+        int result[] = new int[2];
+        for(int i = 0; i<nums.length ;i++){
+            if(nums[i]<target){
+
+
+            }
+        }
+        return result;
+
+    }
+}
