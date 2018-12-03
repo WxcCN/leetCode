cab5bbd865f66fd0f3bd50a0d2479ad03940a2d8
bjwangxingchen <357991863@qq.com>
2018-12-03 18:01:37 +0800

    test

--- src/main/java/algorithms/T5_LongestPalindromicSubstring.java
+++ src/main/java/algorithms/T5_LongestPalindromicSubstring.java
@@ -7,10 +7,13 @@ package algorithms;
 public class T5_LongestPalindromicSubstring {
     public static void main(String[] args) {
         String test1 = "babad";
-        String test2 = "cbbd";
+//        String test2 = "cbbd";
+//        String test2 = "ac";
+//        String test2 = "aaaa";
+        String test2 = "ccc";
 
 
-        System.out.println(longestPalindrome(test1));
+//        System.out.println(longestPalindrome(test1));
         System.out.println(longestPalindrome(test2));
     }
 
@@ -20,40 +23,51 @@ public class T5_LongestPalindromicSubstring {
         //最大长度
         int maxLen = 0;
         //最大子串
-        String sr = null;
+        String sr = "";
 
         char[] chars = s.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
+        for (int i = 0; i < chars.length; i=p+1) {
             p = i;
             int tempLenth = 0;
             String tempS = null;
-            //卫判断
-            if (p + 2 == s.length() - 1) {
-                if ()
+            //结尾卫判断
+            if (p + 1 == s.length()) {
+                if (sr.length() >= 2)
+                    return sr;
+                //已经是最后一位
+                return String.valueOf(chars[p]);
+
             }
             //非回文，指针往后走
-            if (chars[p + 1] != chars[p] && chars[p + 2] != chars[p]) {
+            if (p + 2 < s.length() && chars[p + 1] != chars[p] && chars[p + 2] != chars[p]) {
                 p++;
-            } else if (chars[p + 1] == chars[p]) {
-                //偶回文
-                int j = 1;
-                while (p - j >= 0 && p + j + 1 <= s.length() - 1 && chars[p - j] == chars[p + j + 1]) {
-                    j++;
-                }
-                tempLenth = 2 * j;
-                tempS = s.substring(p - j, p + j + 1);
-            } else if (chars[p + 2] == chars[p]) {
+            }
+            if (p +2 < s.length() && chars[p + 2] == chars[p]) {
                 //奇回文
                 int j = 1;
                 while (p - j >= 0 && p + j + 2 <= s.length() - 1 && chars[p - j] == chars[p + 2 + j]) {
                     j++;
                 }
                 tempLenth = 2 * j + 1;
-                tempS = s.substring(p - j +1 , p + j + 1);
+                tempS = s.substring(p - j + 1, p + j + 2);
+                if (tempLenth > maxLen) {
+                    maxLen = tempLenth;
+                    sr = tempS;
+                }
+
             }
-            if (tempLenth > maxLen) {
-                maxLen = tempLenth;
-                sr = tempS;
+            if (chars[p + 1] == chars[p]) {
+                //偶回文
+                int j = 1;
+                while (p - j >= 0 && p + j + 1 <= s.length() - 1 && chars[p - j] == chars[p + j + 1]) {
+                    j++;
+                }
+                tempLenth = 2 * j;
+                tempS = s.substring(p - j + 1, p + j + 1);
+                if (tempLenth > maxLen) {
+                    maxLen = tempLenth;
+                    sr = tempS;
+                }
             }
         }
         return sr;
